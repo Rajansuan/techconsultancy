5049e1eb7534d3d0072b3bb068d22e9dee8901ae
Rajan Suan <rcs9159@gmail.com>
2025-01-22 20:53:25 +0530

    Cursor animation added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MiddleSection from "./components/MiddleSection"; 
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MiddleSection from "./components/MiddleSection";
+import AnimatedCursor from "react-animated-cursor";
+import "./App.css";
 
 const App = () => {
-    return (
-        <div>
-            <Header />
-            <MiddleSection />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <AnimatedCursor
+        innerSize={8} /* Size of the inner circle */
+        outerSize={35} /* Size of the outer circle */
+        color="35, 59, 151" /* RGB color of the cursor */
+        outerAlpha={0.3} /* Transparency of the outer circle */
+        innerScale={1.5} /* Scale effect when hovering */
+        outerScale={2.5} /* Scale effect when hovering */
+        clickables={[
+          "a", // Links
+          "button", // Buttons
+          ".link", // Custom classes
+        ]}
+      />
+      <Header />
+      <MiddleSection />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,92 @@
 import React, { useState } from "react";
 import "./header.css";
-
+import AnimatedCursor from "react-animated-cursor";
 const Header = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-    return (
-        <div className="header">
-            <div className="logo">Tech Firm</div>
+  return (
+    <div className="header">
+      <AnimatedCursor
+        innerSize={8} /* Size of the inner circle */
+        outerSize={35} /* Size of the outer circle */
+        color="35, 59, 151" /* RGB color of the cursor */
+        outerAlpha={0.3} /* Transparency of the outer circle */
+        innerScale={1.2} /* Scale effect when hovering */
+        outerScale={1.5} /* Scale effect when hovering */
+        clickables={[
+          "a", // Links
+          "button", // Buttons
+          ".link", // Custom classes
+        ]}
+      />
+      <div className="logo">Tech Firm</div>
 
-            <nav>
-                <ul className="nav-links">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="#portfolio">Portfolio</a></li>
-                    <li><a href="#blog">Blog</a></li>
-                </ul>
-            </nav>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+        </ul>
+      </nav>
 
-            <button className="contact-button">Contact Us</button>
+      <button className="contact-button">Contact Us</button>
 
-            <div className="burger" onClick={toggleMenu}>
-                <div className="burger-line"></div>
-                <div className="burger-line"></div>
-                <div className="burger-line"></div>
-            </div>
+      <div className="burger" onClick={toggleMenu}>
+        <div className="burger-line"></div>
+        <div className="burger-line"></div>
+        <div className="burger-line"></div>
+      </div>
 
-            {/* Full screen menu overlay */}
-            {menuOpen && (
-                <div className="fullscreen-menu">
-                    <ul className="fullscreen-nav-links">
-                        <li><a href="#home" onClick={toggleMenu}>Home</a></li>
-                        <li><a href="#about" onClick={toggleMenu}>About</a></li>
-                        <li><a href="#services" onClick={toggleMenu}>Services</a></li>
-                        <li><a href="#portfolio" onClick={toggleMenu}>Portfolio</a></li>
-                        <li><a href="#blog" onClick={toggleMenu}>Blog</a></li>
-                    </ul>
-                </div>
-            )}
+      {/* Full screen menu overlay */}
+      {menuOpen && (
+        <div className="fullscreen-menu">
+          <ul className="fullscreen-nav-links">
+            <li>
+              <a href="#home" onClick={toggleMenu}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about" onClick={toggleMenu}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#services" onClick={toggleMenu}>
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" onClick={toggleMenu}>
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a href="#blog" onClick={toggleMenu}>
+                Blog
+              </a>
+            </li>
+          </ul>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Header;

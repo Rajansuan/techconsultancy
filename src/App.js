bd846de0b0305c0216dfc144a2e8e3e6c2ebfdc9
Rajan Suan <rcs9159@gmail.com>
2025-03-13 20:25:23 +0530

    fix the hamburger menu

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MiddleSection from "./components/MiddleSection";
-import AnimatedCursor from "react-animated-cursor";
 import StatsSection from './components/StatsSection';
 import "./App.css";
 import TrustedByUs from "./components/TrustedByUs";

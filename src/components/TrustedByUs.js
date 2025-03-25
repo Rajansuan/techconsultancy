7721346229da97230b30404fc1a0c297c447c9eb
Rajan Suan <rcs9159@gmail.com>
2025-03-25 22:31:10 +0530

    trusted by us logos added

--- src/components/TrustedByUs.js
+++ src/components/TrustedByUs.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./trustedByUs.css";
-import Logo1 from "../assets/images/logo1.png";
-import Logo2 from "../assets/images/logo2.png";
-// import other logos as needed
+import Logo1 from "../assets/images/logotrust1.png";
+import Logo2 from "../assets/images/logotrust2.png";
+import Logo3 from "../assets/images/logotrust3.svg";
+import Logo4 from "../assets/images/logotrust4.png";
 
 const TrustedByUs = () => {
   return (
@@ -11,9 +12,18 @@ const TrustedByUs = () => {
         <h2 className="trusted-heading">Trusted By</h2>
         <p className="software-business-text">Over 40K+ businesses growing with us</p>
         <div className="logo-container">
-          <img src={Logo1} alt="Client Logo" className="logo" />
-          <img src={Logo2} alt="Client Logo" className="logo" />
-          {/* Add more logos as needed */}
+          <div className="logo-wrapper">
+            <img src={Logo1} alt="Client Logo" className="logo" />
+          </div>
+          <div className="logo-wrapper">
+            <img src={Logo2} alt="Client Logo" className="logo" />
+          </div>
+          <div className="logo-wrapper">
+            <img src={Logo3} alt="Client Logo" className="logo" />
+          </div>
+          <div className="logo-wrapper">
+            <img src={Logo4} alt="Client Logo" className="logo" />
+          </div>
         </div>
       </div>
     </section>
